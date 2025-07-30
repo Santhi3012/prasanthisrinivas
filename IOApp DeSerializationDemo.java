6bec2cf2eec91461fbef5652d842ff24ec75487b
Santhi3012 <228a1a0573@risekrishnasaiprakasam.edu.in>
2025-07-30 10:31:29 +0530

    IOApp DeSerializationDemo.java

---
+++ IOApp DeSerializationDemo.java
@@ -0,0 +1,24 @@
+package com.io;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+
+public class DeSerializationDemo {
+
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+    	//To read from file
+        FileInputStream fis = new FileInputStream("Employeeinfo.txt");
+        
+        //T read from object
+        ObjectInputStream ois= new ObjectInputStream(fis);
+
+		//To read
+        Employee e = (Employee) ois.readObject();
+        System.out.println(e.getEid()+" "+e.getEname()+" "+e.getMblno());
+        
+        //close objects
+        ois.close();
+        fis.close();
+    }
+}
